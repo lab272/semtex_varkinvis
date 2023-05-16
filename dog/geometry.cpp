4dea4667f5cedd04f2bc812b48c932a6c910694f
Hugh Blackburn <hugh.blackburn@gmail.com>
2023-05-16 17:12:33 +1000

    added some cleanups to reduce redundant compiles, from xxt development line

--- dog/geometry.cpp
+++ dog/geometry.cpp
@@ -1,34 +1,38 @@
 ///////////////////////////////////////////////////////////////////////////////
-// geometry.C: define geometrical properties for 2D quad X Fourier spaces.
-//
-// Copyright (c) 1994 <--> $Date$, Hugh Blackburn
+// geometry.cpp: define geometrical properties for 2D quad X Fourier spaces.
 //
 // Most routines are inlined in header file geometry.h.
+//
+// NB: we have capitalized names of all static member data so as to
+// avoid potential conflicts with those instantiated in
+// src/geometry.cpp (which this file is designed to override).
+//
+// Copyright (c) 1994+, Hugh M Blackburn
 ///////////////////////////////////////////////////////////////////////////////
-
-static char RCS[] = "$Id$";
 
 #include <cstdio>
 #include <iostream>
 
 #include <cfemdef.h>
 #include <utility.h>
+#include <veclib.h>
 #include <femlib.h>
 #include <geometry.h>
 
-int_t Geometry::_pid    = 0;
-int_t Geometry::_nproc  = 0;
-int_t Geometry::_np     = 0;
-int_t Geometry::_nz     = 0;
-int_t Geometry::_nzp    = 0;
-int_t Geometry::_nel    = 0;
-int_t Geometry::_psize  = 0;
+int_t Geometry::_Pid    = 0;
+int_t Geometry::_Nproc  = 0;
+int_t Geometry::_Np     = 0;
+int_t Geometry::_Nz     = 0;
+int_t Geometry::_Nzp    = 0;
+int_t Geometry::_Nel    = 0;
+int_t Geometry::_Psize  = 0;
 
-int_t Geometry::_npert  = 0;
-int_t Geometry::_nbase  = 0;
-int_t Geometry::_nslice = 0;
-Geometry::CoordSys Geometry::_csys = Geometry::Cartesian;
-Geometry::Category Geometry::_cat  = Geometry::O2_3D_SYMM;
+int_t Geometry::_Npert  = 0;
+int_t Geometry::_Nbase  = 0;
+int_t Geometry::_Nslice = 0;
+Geometry::CoordSys Geometry::_Csys = Geometry::Cartesian;
+Geometry::Category Geometry::_Cat  = Geometry::O2_3D_SYMM;
+
 
 void Geometry::set (const int_t nel  ,
 		    const int_t npert)
@@ -41,40 +45,43 @@ void Geometry::set (const int_t nel  ,
   static char routine[] = "Geometry::set";
   char        err[StrMax];
 
-  _pid    = Femlib::ivalue ("I_PROC");
-  _nproc  = Femlib::ivalue ("N_PROC");
+  _Pid    = Femlib::ivalue ("I_PROC");
+  _Nproc  = Femlib::ivalue ("N_PROC");
 
-  _np     = Femlib::ivalue ("N_P");
+  _Np     = Femlib::ivalue ("N_P");
 
-  _nbase  = Femlib::ivalue ("N_BASE");
-  _nslice = Femlib::ivalue ("N_SLICE");
-  _csys   = Femlib::ivalue ("CYLINDRICAL") ? 
+  _Nbase  = Femlib::ivalue ("N_BASE");
+  _Nslice = Femlib::ivalue ("N_SLICE");
+  _Csys   = Femlib::ivalue ("CYLINDRICAL") ? 
                      Geometry::Cylindrical : Geometry::Cartesian;
-  _npert  = npert;
-  _nel    = nel;
-  _psize  = nPlane() + (nPlane() % 2);
+  _Npert  = npert;
+  _Nel    = nel;
+  _Psize  = nPlane() + (nPlane() % 2);
 
-  _nz = _nzp = Femlib::ivalue ("N_Z");
+  _Nz = _Nzp = Femlib::ivalue ("N_Z");
 
-  if      (_nbase == 2 && _npert == 2 && _nz == 1) _cat = O2_2D;
-  else if (_nbase == 2 && _npert == 3 && _nz == 1) _cat = O2_3D_SYMM;
-  else if (_nbase == 2 && _npert == 3 && _nz == 2) _cat = O2_3D;
-  else if (_nbase == 3 && _npert == 3 && _nz == 1) _cat = SO2_2D;
-  else if (_nbase == 3 && _npert == 3 && _nz == 2) _cat = SO2_3D;
+  if      (_Nbase == 2 && _Npert == 2 && _Nz == 1) _Cat = O2_2D;
+  else if (_Nbase == 2 && _Npert == 3 && _Nz == 1) _Cat = O2_3D_SYMM;
+  else if (_Nbase == 2 && _Npert == 3 && _Nz == 2) _Cat = O2_3D;
+  else if (_Nbase == 3 && _Npert == 3 && _Nz == 1) _Cat = SO2_2D;
+  else if (_Nbase == 3 && _Npert == 3 && _Nz == 2) _Cat = SO2_3D;
   else {
     sprintf (err, "illegal: N_BASE = %1d, N_PERT = %1d, N_Z = %1d",
-	     _nbase, _npert, _nz); message (routine, err, ERROR);
+	     _Nbase, _Npert, _Nz); message (routine, err, ERROR);
   }
 
   // -- We can only allow SO2_2D if BETA != 0.
 
-  if ((_cat == SO2_2D) && (Femlib::value ("BETA") > EPSDP))
-    message (routine, "SO2_2D needs BETA=0, i.e. z-invariance, too.", ERROR);
+  if ((_Cat == SO2_2D) && (Femlib::value ("BETA") > EPSDP))
+    message
+      (routine, "SO2_2D needs BETA=0, i.e. z-invariance, too.", ERROR);
 
   // -- Other sanity checks.
 
-  if (_nproc  > 1) message (routine, "serial execution only",          ERROR);
-  if (_nslice < 1) message (routine, "N_SLICE must be set in session", ERROR);
+  if (_Nproc  > 1)
+    message (routine, "serial execution only",            ERROR);
+  if (_Nslice < 1)
+    message (routine, "N_SLICE must be set in session",   ERROR);
 }
 
 
@@ -83,7 +90,7 @@ const char* Geometry::symmetry ()
 // Return a string value of the spatial symmetry category of problem.
 // ---------------------------------------------------------------------------
 {
-  switch (_cat) {
+  switch (_Cat) {
   case O2_2D:      return "O(2), 2D"; break;
   case O2_3D_SYMM: return "O(2), 3D, standing wave"; break;
   case O2_3D:      return "O(2), 3D, travelling wave"; break;
